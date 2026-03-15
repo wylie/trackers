736a1e82e211b70740f87aa2db0126b90454c12e
Wylie Fisher <me@wyliefisher.com>
2026-03-15 18:06:28 -0400

    Add Astro sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	site: 'https://www.simpletrackers.io',
+	integrations: [tailwind(), sitemap()],
 	adapter: vercel(),
 	output: 'server',
 });

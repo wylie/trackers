2cb28b2f97cf59b3b9929b32d36f74a7b8b1058b
Wylie Fisher <me@wyliefisher.com>
2026-03-15 18:12:40 -0400

    Use standalone sitemap.xml without sitemap index

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.simpletrackers.io',
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind()],
 	adapter: vercel(),
 	output: 'server',
 });
